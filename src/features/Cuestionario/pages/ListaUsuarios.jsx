2f17d2a1ca8b65f05a0db745a4fe66b33167cfdf
jst0wen <owenpinzon1@gmail.com>
2023-08-29 21:14:44 -0500

    subiendo cambios

--- src/features/Cuestionario/pages/ListaUsuarios.jsx
+++ src/features/Cuestionario/pages/ListaUsuarios.jsx
@@ -1,79 +1,36 @@
-import { Delete, Edit } from "@mui/icons-material";
-import { Box, Drawer, IconButton, TextField } from "@mui/material";
-import { DataGrid, GridToolbar, esES } from "@mui/x-data-grid";
-import React, { useState } from "react";
-import CrearCuestionarioPage from "./CrearCuestionarioPage";
-import useObtenerCuestionarios from "../hooks/useObtenerCuestionarios";
-import { query } from "firebase/firestore";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 function ListaUsuarios() {
-  const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(5);
-
-  const queryCuestionrios = useObtenerCuestionarios();
-  const [open, setOpen] = useState(false);
-  const abrirDrawer = () => {
-    setOpen(true);
-  };
-  const columns = [
-    { field: "nombre", headerName: "Nombre", width: 300 },
-    { field: "codigo", headerName: "Codigo", width: 300 },
-    { field: "opciones", headerName: "Opciones", width: 300 },
-   
-  ];
-
-  const handlePageChange = (params) => {
-    setPage(params.page);
-  };
-
-  const handlePageSizeChange = (params) => {
-    console.log("Params ", params);
-    setPageSize(params.pageSize);
-  };
-
-  const dataCuestionarios = React.useMemo(() => {
-    if (queryCuestionrios.data?.data) {
-      return queryCuestionrios.data?.data.map((cuestionario) => ({
-        ...cuestionario,
-        id: cuestionario._id,
-      }));
-    }
-    return [];
-  }, [queryCuestionrios.data?.data]);
-
+  
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <DataGrid
-        rows={dataCuestionarios || []}
-        columns={columns}
-        loading={queryCuestionrios.isFetching}
-        pageSize={pageSize}
-        page={page}
-        onPageChange={handlePageChange}
-        onPageSizeChange={handlePageSizeChange}
-        localeText={esES.components.MuiDataGrid.defaultProps.localeText}
-        components={{
-          Toolbar: GridToolbar,
-        }}
-      />
-      <Drawer
-        anchor={"right"}
-        open={open}
-        onClose={() => {
-          setOpen(false);
-        }}
-        PaperProps={{
-          style: { width: "100%", padding: "25px", maxWidth: "700px" },
-        }}
-      >
-        <h3 className="font-bold text-[20px]">Editar cuestionario</h3>
-        <hr className="mb-5" />
-        <CrearCuestionarioPage />
-      </Drawer>
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Nombre</TableCell>
+            <TableCell align="right">Codigo</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell>1</TableCell>
+            <TableCell align="right">Juan</TableCell>
+            <TableCell align="right">25</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>2</TableCell>
+            <TableCell align="right">María</TableCell>
+            <TableCell align="right">30</TableCell>
+          </TableRow>
+         
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
+
 
 export default ListaUsuarios;
